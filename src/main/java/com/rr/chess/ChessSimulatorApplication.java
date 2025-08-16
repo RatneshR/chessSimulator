51740dcecabcc013bcb12949bf7fe2fa49b258c7
Ratnesh Rai <ratnesh.rai26@gmail.com>
2025-08-17 01:18:47 +0530

    Application Starting logic

--- src/main/java/com/rr/chess/ChessSimulatorApplication.java
+++ src/main/java/com/rr/chess/ChessSimulatorApplication.java
@@ -1,13 +1,42 @@
 package com.rr.chess;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class ChessSimulatorApplication {
+public class ChessSimulatorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChessSimulatorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// This method can be used to execute code after the application has started
+		System.out.println("Chess Simulator Application has started successfully!");
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter input(eg. King E5): ");
+		String input = scanner.nextLine().trim();
+
+		try {
+			String[] parts = input.split("\\s+");
+			if (parts.length != 2) {
+				throw new IllegalArgumentException("Input must be in the format 'Piece Position' (e.g., 'King E5').");
+			}
+			String pieceType = parts[0];
+			String position = parts[1];
+
+			// Here you can add logic to handle the piece type and position - Create a Movement Service
+
+		} catch (Exception e) {
+			System.out.println("An error occurred: " + e.getMessage());
+		} finally {
+			scanner.close();
+		}
 	}
 
 }
