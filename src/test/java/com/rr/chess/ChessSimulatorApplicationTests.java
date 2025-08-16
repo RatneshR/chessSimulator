b27639e3d427a48a19642a67a63678138f23a9ca
Ratnesh Rai <ratnesh.rai26@gmail.com>
2025-08-17 01:04:24 +0530

    Initial Commit

---
+++ src/test/java/com/rr/chess/ChessSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rr.chess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
